cd57e39ebfa3f6ad1611fe19de1638d96e5cb1bc
Ryo <ryo3110yamanashi@gmail.com>
2020-11-06 20:53:18 +0900

    UserMigarte,ViewHeader

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,11 +1,12 @@
 <div class="contaier">
     <div class="row ">
-        <div class="col-md-4 title">
+        <div class="col-md-6 title">
             <a href={{ route('member.list') }}>{{ $title }}</a>
         </div>
-        <div class="row col-md-7 justify-content-around align-items-center">
-                <p>ユーザー： {{ Auth::user()->name }}</p>
-                <p>最終ログイン： {{ Auth::user()->last_login_at }}</p>
+        <div class="row col-md-5 justify-content-around align-items-center">
+            <div><a href={{ route('home') }} class="btn btn-primary">Home</a></div>
+            <p>ユーザー： {{ Auth::user()->name }}</p>
+            <p>最終ログイン： {{ Auth::user()->last_login_at }}</p>
         </div>
     </div>
 </div>
